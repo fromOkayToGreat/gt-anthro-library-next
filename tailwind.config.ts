872319e1ac09d2f21d642052c3179ea567fba48a
Francisco <francisco@fromokaytogreat.com>
2024-01-02 18:17:19 -0800

    🐛 extend the theme, not replace default colors palette entirely
    Replace this part:
    
    `theme: {
      colors: {
        // your colors
      },
    },
    `
    With this:
    `theme: {
      extend: {
        colors: {
          // your colors
        },
      },
    },
    `

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,14 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      text: "hsl(0, 0%, 2%)",
-      background: "hsl(39, 71%, 95%)",
-      primary: "hsl(358, 100%, 32%)",
-      secondary: "hsl(0, 79%, 87%)",
-      accent: "hsl(0, 78%, 47%)",
+    extend: {
+      colors: {
+        text: "hsl(0, 0%, 2%)",
+        background: "hsl(39, 71%, 95%)",
+        primary: "hsl(358, 100%, 32%)",
+        secondary: "hsl(0, 79%, 87%)",
+        accent: "hsl(0, 78%, 47%)",
+      },
     },
   },
   plugins: [],
