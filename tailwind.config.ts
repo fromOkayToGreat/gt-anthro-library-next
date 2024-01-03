db0e3db0ada174f798ef19f086b5d56791bc7fec
Francisco <francisco@fromokaytogreat.com>
2024-01-02 18:07:29 -0800

    💄 add colors to theme in tailwind.config.ts
    `colors: {
      'text': 'hsl(0, 0%, 2%)',
      'background': 'hsl(39, 71%, 95%)',
      'primary': 'hsl(358, 100%, 32%)',
      'secondary': 'hsl(0, 79%, 87%)',
      'accent': 'hsl(0, 78%, 47%)',
     },`

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,15 @@ const config: Config = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    colors: {
+      text: "hsl(0, 0%, 2%)",
+      background: "hsl(39, 71%, 95%)",
+      primary: "hsl(358, 100%, 32%)",
+      secondary: "hsl(0, 79%, 87%)",
+      accent: "hsl(0, 78%, 47%)",
+    },
+  },
   plugins: [],
 };
 
